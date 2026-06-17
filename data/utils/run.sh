6a947aa7ef5bc1c0ad24bafed4ee839c9726bfda
algarfer <algarferx@proton.me>
2026-06-18 00:49:43 +0200

    chore: added stub shapes and datasets to check correct engines behavior

---
+++ data/utils/run.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+printf "[+] Running rudof/$1:latest with the conformant data file\n\n"
+
+docker run \
+  --rm \
+  -v ./test-shapes.ttl:/shapes.ttl:ro \
+  -v ./data-conforms.ttl:/data.ttl:ro \
+  rudof/$1:latest \
+  /data.ttl \
+  turtle \
+  /shapes.ttl \turtle \
+  /test.csv \
+  /dev/stdout
+
+printf "\n\n\n[+] Running rudof/$1:latest with the non-conformant data file\n\n"
+
+docker run \
+  --rm \
+  -v ./test-shapes.ttl:/shapes.ttl:ro \
+  -v ./data-no-conforms.ttl:/data.ttl:ro \
+  rudof/$1:latest \
+  /data.ttl \
+  turtle \
+  /shapes.ttl \turtle \
+  /test.csv \
+  /dev/stdout
