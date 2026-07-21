35fa7371e2bcb1de3b3a83d4106676e9d48fdfa2
algarfer <algarferx@proton.me>
2026-07-21 13:08:59 +0200

    docs(data): documented icdd patches

--- data/utils/patch/icdd.py
+++ data/utils/patch/icdd.py
@@ -20,16 +20,27 @@ def patch(mode: str, filename: str, other: list[str]) -> None:
     os.replace(tmp.name, filename)
 
 def __patch_data(line: str) -> str:
-    line = re.sub(
+    line = __patch_xsd_dateTime(line)
+    line = __patch_xsd_boolean(line)
+    return line
+
+def __patch_shapes(line: str) -> str:
+    return line
+
+# Transforms dateTimes with format "dd.MM.YYYY HH:MM:SS"
+# into xsd:dateTime format: "YYYY-MM-ddTHH:MM:SS"
+def __patch_xsd_dateTime(line: str) -> str:
+    return re.sub(
         r"(?P<day>\d\d)\.(?P<month>\d\d)\.(?P<year>\d\d\d\d) (?P<hours>\d\d):(?P<minutes>\d\d):(?P<seconds>\d\d)",
         r"\g<year>-\g<month>-\g<day>T\g<hours>:\g<minutes>:\g<seconds>",
         line
     )
+
+# Transforms "True"^^xsd:boolean into the correct version:
+# "true"^^xsd:boolean
+def __patch_xsd_boolean(line: str) -> str:
     return re.sub(
         r'"True"\^\^xsd:boolean',
         r'"true"^^xsd:boolean',
         line
     )
-
-def __patch_shapes(line: str) -> str:
-    return line
